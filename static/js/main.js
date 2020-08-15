a918c17460e6a3378de9038c6a5560597d99c525
Bush <matthew.bush@siemens.com>
2020-08-15 02:52:27 +0100

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,103 @@
+/**
+ * 
+ * Nav toggle
+ */
+
+(function() {
+
+  var hamburger = {
+    navToggle: document.querySelector('.nav-toggle'),
+    nav: document.querySelector('nav'),
+    navClose: document.querySelector('.svg-close'),
+
+    
+
+    doToggle: function(e) {
+      e.preventDefault();
+      this.navToggle.classList.toggle('expanded');
+      this.nav.classList.toggle('expanded');
+      this.navClose.classList.toggle('expanded');
+    }
+  };
+ 
+
+  hamburger.navToggle.addEventListener('click', function(e) { 
+    hamburger.doToggle(e); });
+  hamburger.navClose.addEventListener('click', function(e) { 
+    hamburger.doToggle(e); });
+
+}());
+
+/**
+ * 
+ * aos
+ * Animate on scroll inititiate and settings
+ */
+
+AOS.init({
+  duration: 600, // values from 0 to 3000, with step 50ms
+  once: true,
+});
+
+
+/**
+ * 
+ * things about me slider
+ */
+
+if(document.querySelector('.slide-btn')){
+  var thingsSlides = document.querySelectorAll('.things-slider .slider-item');
+  var currentSlide = 0;
+  // var slideInterval = setInterval(nextSlide,2000);
+  
+  function nextSlide() {
+      
+      thingsSlides[currentSlide].className = 'slider-item';
+      currentSlide = (currentSlide+1)%thingsSlides.length;
+      thingsSlides[currentSlide].className = 'slider-item showing';
+      
+  }
+  document.querySelector('.slide-btn').addEventListener('click', nextSlide)
+
+}
+
+/**
+ * 
+ * testimonial slider. Services.html
+ */
+
+if(document.querySelector('.circle-slide-btn')){
+  var slides = document.querySelectorAll('.testimonials-slider .testimonial-item');
+  var currentSlide2 = 0;
+  // var slideInterval = setInterval(nextSlide,2000);
+  
+  function nextSlide2() {
+      slides[currentSlide2].className = 'testimonial-item slide';
+      currentSlide2 = (currentSlide2+1)%slides.length;
+      slides[currentSlide2].className = 'testimonial-item slide test-showing';
+      
+  }
+  document.querySelector('.circle-slide-btn').addEventListener('click', nextSlide2)
+}
+
+/**
+ * Home hero auto slider 
+ */
+
+var heroSlides = document.querySelectorAll('.home-hero .home-hero-img');
+var currentHeroSlide = 0;
+
+if(heroSlides.length > 0){
+  console.log(heroSlides)
+  setInterval(function() {
+    heroSlides[currentHeroSlide].className = 'home-hero-img';
+    currentHeroSlide = (currentHeroSlide+1)%heroSlides.length;
+    heroSlides[currentHeroSlide].className = 'home-hero-img hero-showing';
+    
+    if (currentHeroSlide >= heroSlides.length) {
+      currentHeroSlide = 0;
+    }
+}, 4000);
+}
+
+
